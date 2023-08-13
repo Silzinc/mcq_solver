be37ffa34ce66fb0a2a72d82f08f62c7707b8fa9
Jonas Bloch <jonasbloch@pop-os.localdomain>
2023-08-13 17:35:38 +0200

    Testing script

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+#![allow(unused_imports)]
+
+mod bool_tables;
+use bool_tables::BoolTable;
+
+mod mcq;
+use mcq::*;
+use Answer::*;
+
+mod solver;
+
 fn main() {
-    println!("Hello, world!");
+    let mcq = MCQ::generate_random();
+    for k in 0..100 {
+        if Sheet::generate_random(&mcq).grade == mcq::NUMBER_OF_QUESTIONS as u8 {
+            println!("Max grade found at sheet {}/100", k);
+            break;
+        }
+    }
+
+    // let _array = [[0u8; 24]; 1 << 18];
+    // println!("{} bytes allocated on a big array\n{} bytes remaining on the stack", 
+    // std::mem::size_of_val(&_array), stacker::remaining_stack().unwrap());
+    println!("{}", std::mem::size_of::<Option<[i32; 2]>>());
+    println!("Solver weighs {}/{} available bytes", 
+    std::mem::size_of::<solver::Solver>(), stacker::remaining_stack().unwrap());
 }
