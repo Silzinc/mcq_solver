69be0afd3c0b19ad5f5553176a065190fecaffc7
Jonas Bloch <jonasbloch@pop-os.localdomain>
2023-08-16 13:49:46 +0200

    Introduced a struct holding annealing parameters with default implementation

--- src/parameters.rs
+++ src/parameters.rs
@@ -1,7 +1,22 @@
-/* Parameters */
-pub const NUMBER_OF_QUESTIONS: usize = 128 ; // Chosen as a power of 2 on purpose to use SIMD
-pub const SIMULATION_SHEETS  : usize = 800 ;
-pub const POSSIBLE_ANSWERS   : u8    = 8   ;
-pub const STARTING_BETA      : f64   = 0.1 ;
-pub const MAX_BETA           : f64   = 0.5 ;
-pub const LAMBDA_INV         : f64   = 1.01;
+#[derive(Clone, Debug)]
+pub struct AnnealingParameters {
+    pub number_of_questions : usize,
+    pub simulation_sheets   : usize,
+    pub possible_answers    : u8   ,
+    pub starting_beta       : f64  ,
+    pub max_beta            : f64  ,
+    pub lambda_inv          : f64  ,
+}
+
+impl std::default::Default for AnnealingParameters {
+    fn default() -> Self {
+        Self {
+            number_of_questions : 120 ,
+            simulation_sheets   : 500 ,
+            possible_answers    : 8   ,
+            starting_beta       : 0.1 ,
+            max_beta            : 0.5 ,
+            lambda_inv          : 1.01,
+        }
+    }
+}
