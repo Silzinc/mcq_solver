0889b0828bb69fd2dd32d9c91e44a7a44b713979
Jonas Bloch <jonasbloch@pop-os.localdomain>
2023-08-16 11:01:25 +0200

    Separated Sheet module from MCQ module.

--- src/mcq.rs
+++ src/mcq.rs
@@ -1,22 +1,8 @@
-use crate::parameters::{NUMBER_OF_QUESTIONS, POSSIBLE_ANSWERS};
+use crate::{
+    parameters::{NUMBER_OF_QUESTIONS, POSSIBLE_ANSWERS},
+    sheet::*,
+};
 use rand::{thread_rng, Rng};
-
-pub type Answer = u8;
-
-#[derive(Clone, Debug)]
-pub struct Sheet {
-    pub answers: [Answer; NUMBER_OF_QUESTIONS],
-    pub grade: u8,
-}
-
-impl std::default::Default for Sheet {
-    fn default() -> Self {
-        Self {
-            answers: [0 as Answer; NUMBER_OF_QUESTIONS],
-            grade: 0u8,
-        }
-    }
-}
 
 pub struct MCQ {
     pub answers: [Answer; NUMBER_OF_QUESTIONS],
