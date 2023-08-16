5b38db18f0a9ef7b794e4497b60424f4972fcb19
Jonas Bloch <jonasbloch@pop-os.localdomain>
2023-08-16 13:50:37 +0200

    Switched to structured paramters

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,14 @@ use annealing::AnnealingSolver;
 use parameters::*;
 
 fn main() {
-    let mut time = 0f64;
+    let params = AnnealingParameters::default();
+    let mut _time = 0f64;
     let mut iter = 0u32;
     let mut _successes = 0u32;
 
     while iter < 1u32 {
-        let mcq = MCQ::generate_random();
-        let sheets: Vec<Sheet> = vec_from_fn(SIMULATION_SHEETS, || mcq.generate_random_sheet());
+        let mcq = MCQ::generate_random(&params);
+        let sheets: Vec<Sheet> = vec_from_fn(params.simulation_sheets, || mcq.generate_random_sheet());
         let best_grade = sheets.iter().max_by_key(|sh| sh.grade).unwrap().grade;
         iter += 1;
         let now = std::time::Instant::now();
@@ -26,14 +27,14 @@ fn main() {
             _successes += 1;
             println!(
                 "My grade : {0}/{0} \nBest grade of {1} answer sheets : {2}/{0}\nPossible choices : {3}\nTime required : {4} ms", 
-                NUMBER_OF_QUESTIONS,  
-                SIMULATION_SHEETS, 
+                params.number_of_questions,  
+                params.simulation_sheets, 
                 best_grade,
-                POSSIBLE_ANSWERS,
+                params.possible_answers,
                 now.elapsed().as_nanos() as f64 / 1_000_000.
             );
         }
-        time += now.elapsed().as_nanos() as f64 / 1_000_000.;
+        _time += now.elapsed().as_nanos() as f64 / 1_000_000.;
     }
 
     // println!("Solved {}/{} MCQ's in {:.2e} seconds", successes, iter, time / 1_000.);
