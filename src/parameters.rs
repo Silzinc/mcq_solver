b2dc4aca0aa6b87ed9ec2ee20a6e3fada1593404
Silzinc <jonasbloch85@gmail.com>
2023-08-18 11:29:31 +0200

    Removed Default impl

--- src/parameters.rs
+++ src/parameters.rs
@@ -7,16 +7,3 @@ pub struct AnnealingParameters {
     pub max_beta: f64,
     pub lambda_inv: f64,
 }
-
-impl std::default::Default for AnnealingParameters {
-    fn default() -> Self {
-        Self {
-            number_of_questions: 120,
-            simulation_sheets: 500,
-            possible_answers: 8,
-            starting_beta: 0.1,
-            max_beta: 0.5,
-            lambda_inv: 1.01,
-        }
-    }
-}
