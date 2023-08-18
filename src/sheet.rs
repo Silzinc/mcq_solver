7166a83871ef1f9f23805693fcf17c8e6edfa5cb
Silzinc <jonasbloch85@gmail.com>
2023-08-18 11:29:28 +0200

    Removed unnecessary comments and macros

--- src/sheet.rs
+++ src/sheet.rs
@@ -5,14 +5,3 @@ pub struct Sheet {
     pub answers: Vec<Answer>,
     pub grade: u8,
 }
-
-/*
-impl std::default::Default for Sheet {
-    fn default() -> Self {
-        Self {
-            answers: vec![0 as Answer; NUMBER_OF_QUESTIONS],
-            grade: 0u8,
-        }
-    }
-}
-*/
